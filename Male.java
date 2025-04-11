a6c00fd71fe94401b00a835d98b3081c13a16f95
Meshaki123 <meshakieliaza0@gmail.com>
2025-04-11 21:31:10 +0300

    Initial Project Upload

---
+++ Male.java
@@ -0,0 +1,12 @@
+public class Male {
+        String Name;
+        String Voice;
+        String Gender;
+
+        void DisplayMale() {
+            System.out.println("\nThese are Male Properties");
+            System.out.println("Name: " + Name);
+            System.out.println("Voice: " + Voice);
+            System.out.println("Gender: " + Gender);
+        }
+    }
