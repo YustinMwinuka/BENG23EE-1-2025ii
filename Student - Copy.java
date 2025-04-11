52fa6c8a00f8f7fc36a3c491fdafe72c7fef0387
Meshaki123 <meshakieliaza0@gmail.com>
2025-04-11 22:00:25 +0300

    Added Project Number Two

---
+++ Student - Copy.java
@@ -0,0 +1,19 @@
+public class Student {
+    String Name;
+    String Gender;
+    String Course;
+
+    // Constructor
+    Student(String Name, String Gender, String Course) {
+        this.Name = Name;
+        this.Gender = Gender;
+        this.Course = Course;
+    }
+
+    void displayStudent() {
+        System.out.println("Student Name: " + Name);
+        System.out.println("Student Gender: " + Gender);
+        System.out.println("Student Course: " + Course);
+    }
+}
+
