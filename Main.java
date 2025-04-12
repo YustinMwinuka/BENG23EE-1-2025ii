f155c698f7269b7366eb496ab68b4d1cc1c56944
Meshaki123 <meshakieliaza0@gmail.com>
2025-04-12 16:16:57 +0300

    GROUP NUMBER 25-PROJECT ONE

--- Main.java
+++ Main.java
@@ -1,30 +1,26 @@
- import java.util.Scanner;
-class SOMA{
-    public static void main(String[]args){
-          String Name;
-       program p=new program("OOP",45690,12);
-       student s1=new student( "JUSTICE",343401743,2);
-       student s2=new student("ZAINA",343402213,2);
-       System.out.println("Student info");
-       s1.DisplayStudent();
-       System.out.println(" ");
-       s2.DisplayStudent();
-       System.out.println("Subject info");
-        System.out.println(" ");
-        p.DisplayProgram();
-       Scanner sc=new Scanner(System.in);
-       System.out.println("Enter the name");
-       Name=sc.nextLine();
-       if(Name.equalsIgnoreCase(s1.Name)){
-            System.out.println("Machine language");
-       }
-       else if (Name.equalsIgnoreCase(s2.Name) ) {
-            System.out.println("BENGCOE");
+import java.util.Scanner;
+public class Main {
+    static String Name;
+  static   String Gender;
+   static String Voice;
 
-       }
-       else{
-            System.out.println("none of the option");
+    public static void main(String[] args){
+        male omale=new male();
+        female ofemale= new female();
+        int choice;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("there are two choices \n 1:male \n 2:female");
+        System.out.println("Enter your choice");
+        choice=sc.nextInt();
+        switch(choice){
+            case 1:
+                 System.out.println("your choice is male");
+              omale.Displaymale();
+                break;
+            case 2:
+                ofemale.Displaymale();
+                break;
 
-       }
-     }
+        }
+    }
 }
