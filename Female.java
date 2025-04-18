be9bdda7d420a130c0f66118e51ba21925a0b6c9
Meshaki123 <meshakieliaza0@gmail.com>
2025-04-18 06:13:42 +0300

    GROUP NUMBER 12

---
+++ Female.java
@@ -0,0 +1,12 @@
+public class Female {
+    String Name;
+    String Voice;
+    String Gender;
+
+    void DisplayFemale() {
+        System.out.println("\nThese are Female Properties");
+        System.out.println("Name: " + Name);
+        System.out.println("Voice: " + Voice);
+        System.out.println("Gender: " + Gender);
+    }
+}
